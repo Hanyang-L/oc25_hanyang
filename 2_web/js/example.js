3fe16e50321a40252a98532eb46b832cb0b30837
Hanyang-L <linhany02@icloud.com>
2025-11-11 09:09:51 +0100

    js.1

---
+++ 2_web/js/example.js
@@ -0,0 +1,23 @@
+var message = "bienvenue";
+
+console.log(message);
+
+function f1() {
+    console.log(f1);
+}
+
+//f1();
+
+console.log(document.querySelector('.important'))
+console.log(document.getElementById('.bienvenue'))
+//for (var i=0; i<10; i++) {
+    //i++ = i=i+1 
+//   console.log(1);
+//}
+function ajouter_perso() {
+    var nom_perso='Francois'
+    var nouveau_li = document.createElement('li');
+    var nouveau_txt=document.createTextNode("perso");
+
+    console.log('perso');
+}
