7939b8d5e9919f78369c4abcb04999103d11875d
Hanyang-L <linhany02@icloud.com>
2025-11-20 14:07:36 +0100

    Update example_2.js

--- 2_web/js/example_2.js
+++ 2_web/js/example_2.js
@@ -10,7 +10,25 @@ ctx_statut.fillRect(0,0,300,300);
 const dessin = document.getElementById("dessin");
 const context = dessin.getContext("2d");
 
+context.beginPath();
+context.strokeStyle = "red";
+context.moveTo(250, 250);
+context.lineTo(100, 250);
+context.lineTo(250, 400);
+context.lineTo(400, 250);
+context.closePath();
 context.fillStyle = "green";
-context.fillRect(0,0,200,100);
-context.fillRect(200,200,100,100);
+context.fillStyle();
+context.stroke();
+
+context.beginPath();
+context.arc(250, 250, 100  , 0, 2 * Maths.PI);
+// (coordonné x du centre, coordoné y du centre, 
+// rayon en px, angle de départ, angle en RAD)
+context.fillStyle = "red";
+context.fill();
+
+// context.fillStyle = "green";
+// context.fillRect(0,0,200,100);
+// context.fillRect(200,200,100,100);
 
